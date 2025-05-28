3bab5d6d12a64b706f686e60261f45a1e0ee880c
gft.lucas <gft.lucas@grupofleury.com.br>
2025-05-28 16:16:43 -0300

    conceitos e exercicios

--- testes/01-testes.js
+++ testes/01-testes.js
@@ -2,10 +2,31 @@
  describe -> agrupador de testes
  it       -> implementação do teste
 
+ TDD
+   crio o teste
+   vejo o teste falhar
+   crio a implementação para o teste passar
+   rodo o teste
+   refatoro o código para melhorar...
+
+   ASSERTION/ASSERCAO
+      verificar se um comportamento está de acordo com o esperado
+
  */
 
+   import { exibirNomeDogFormatado } from '../conceitos/testes-de-unidade.js'
+   import assert from 'node:assert'
+
  describe('Testes do Projeto', () => {
-    it ('primeiro teste', () => {
+   it(`deve exibir o nome do dog com letras maiúsculas`, () => {
+      assert.strictEqual(exibirNomeDogFormatado('Mimosa'), 'MIMOSA')
+      // verifique se: <valor atual> É IGUAL A <valor esperado>
+   })
+    it('primeiro teste', () => {
         
+    })
+
+    it('Segundo teste', () => {
+      throw new Error()
     })
  })
